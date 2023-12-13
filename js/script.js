0081f918f7c736124747a00eca39f995361b2163
renanolv7 <reviniciusoliveira@gmail.com>
2023-12-13 00:40:04 -0300

    feat: update script

--- js/script.js
+++ js/script.js
@@ -9,8 +9,14 @@ function insert(numero) {
 }
 
 function click_sinal(operador) {
-    if (valorTela && !operacaoPendente) {
-        valorAnterior = valorTela;
+    if (valorTela) {
+        if (!operacaoPendente) {
+            valorAnterior = valorTela;
+        } else {
+            // Se já houver uma operação pendente, execute a operação
+            executa_igual();
+        }
+
         operacao = operador;
         valorTela = "";
         operacaoPendente = true;
@@ -20,13 +26,34 @@ function click_sinal(operador) {
 
 function executa_igual() {
     if (valorTela && operacaoPendente) {
-        valorTela = eval(`${parseFloat(valorAnterior)} ${operacao} ${parseFloat(valorTela)}`);
+        let resultado;
+
+        switch (operacao) {
+            case "+":
+                resultado = parseFloat(valorAnterior) + parseFloat(valorTela);
+                break;
+            case "-":
+                resultado = parseFloat(valorAnterior) - parseFloat(valorTela);
+                break;
+            case "*":
+                resultado = parseFloat(valorAnterior) * parseFloat(valorTela);
+                break;
+            case "/":
+                resultado = parseFloat(valorAnterior) / parseFloat(valorTela);
+                break;
+
+            default:
+                break;
+        }
+
+        valorTela = resultado.toString(); // Atualizar valorTela com o resultado
         operacao = "";
-        valorAnterior = "";
+        valorAnterior = valorTela; // Atualizar valorAnterior para a próxima operação
         operacaoPendente = false;
-        document.getElementById("tela").value = valorTela;
+        document.getElementById("tela").value = valorTela;;
     }
 }
+
 
 function reset() {
     valorTela = "";
